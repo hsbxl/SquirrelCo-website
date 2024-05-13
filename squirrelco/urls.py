dfbf8fa533c335d6baa1da604b8ca096bb995690
altf4arnold <arnold.dech@adct.be>
2024-05-14 00:58:38 +0200

    [ADD] Primary config for templates rendering

--- squirrelco/urls.py
+++ squirrelco/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import Index
 
 urlpatterns = [
+    path("", Index.as_view(), name="Index"),
     path('admin/', admin.site.urls),
 ]
